49bc0d2987fc91317d63ff02b95d66f154a3b60a
Binhchuoizzz <binhndhe161032@gmail.com>
2024-11-12 15:35:36 +0700

    Xong Work

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 import About from "@/components/About";
 import Journey from "@/components/Journey";
-import Work from "@/components/Work";
+import Work from "@/components/Work/Work";
 import Contact from "@/components/Contact";
 import { useEffect } from "react";
 import FixedMenu from "@/components/FixedMenu";
